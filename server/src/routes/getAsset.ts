22c060299c46cacdc737c4e99622ba876525875a
Vitor Windberg <vitorwlima13@gmail.com>
2023-08-23 20:38:58 -0300

    feat: add normal ratios

--- server/src/routes/getAsset.ts
+++ server/src/routes/getAsset.ts
@@ -1,4 +1,4 @@
-import { endOfYesterday } from 'date-fns'
+import { startOfYesterday } from 'date-fns'
 import { FastifyInstance } from 'fastify'
 import { fetchMarket } from 'src/utils/fetchMarket'
 
@@ -6,19 +6,31 @@ export const getAsset = async (fastify: FastifyInstance) => {
   fastify.get<{ Params: { asset: string } }>(
     '/:asset',
     async (request, reply) => {
-      const yesterdayOrFridayIfWeekend = endOfYesterday()
+      const yesterdayOrFridayIfWeekend = startOfYesterday()
         .toISOString()
         .split('T')[0]
-      const { data: ratios } = await fetchMarket(
+      const { data: allMarketRatios } = await fetchMarket(
         `companies/${request.params.asset}/market_ratios?period_init=${yesterdayOrFridayIfWeekend}`,
+      )
+
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+
+      const { data: allRatios } = await fetchMarket(
+        `companies/${request.params.asset}/ratios`,
+      )
+
+      const marketRatios = allMarketRatios.find(
+        (ratio: any) => ratio.ticker === request.params.asset,
       )
 
       const data = {
         name: 'Nome da empresa', // fazer obj pra nao precisar fazer fetch disso
+        ...marketRatios,
+        allRatios,
       }
 
       reply.code(200)
-      return { ratios }
+      return data
     },
   )
 }
