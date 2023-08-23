4ae896a5e75000b7744a0755bb62baf9fd44fe67
Vitor Lima <vitorwlima13@gmail.com>
2023-08-22 22:43:35 -0300

    feat: get PVP and PL

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
 import yahooFinance from "yahoo-finance2";
 
 const test = async () => {
-  const result = await yahooFinance.quoteSummary("PETR4.SA", { modules: ["financialData"] })
-  console.dir(result, { depth: null })
+  const quoteResult = await yahooFinance.quote(`BLAU3.SA`);
+  const quoteSummaryResult = await yahooFinance.quoteSummary(`BLAU3.SA`, {
+    modules: ["financialData"],
+  });
+
+  const pvp = quoteResult.priceToBook;
+  const pl = quoteResult.trailingPE;
+
+  console.dir({ quoteResult, quoteSummaryResult, pvp, pl }, { depth: null });
 };
 
-test()
+test();
