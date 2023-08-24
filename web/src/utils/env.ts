64d803c965d6bd2cd21b441e3ca20a410f59551b
Vitor Lima <vitorwlima13@gmail.com>
2023-08-24 00:38:25 -0300

    fix: nextjs typesafe env vars

--- web/src/utils/env.ts
+++ web/src/utils/env.ts
@@ -4,4 +4,6 @@ const envSchema = z.object({
   NEXT_PUBLIC_SERVER_URL: z.string(),
 })
 
-export const ENV = envSchema.parse(process.env)
+export const ENV = envSchema.parse({
+  NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL,
+})
