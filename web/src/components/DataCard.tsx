7c00df2bd466effd52d54d9bad654c31ca3720ac
Vitor Lima <vitorwlima13@gmail.com>
2023-08-31 21:04:40 -0300

    feat: use formatted data

--- web/src/components/DataCard.tsx
+++ web/src/components/DataCard.tsx
@@ -1,13 +1,10 @@
 type Props = {
   title: string
-  data: string | number | null
+  data: string
   isLast: boolean
 }
 
 export const DataCard: React.FC<Props> = ({ title, data, isLast }) => {
-  const formattedData =
-    typeof data === 'string' ? data : data?.toFixed(2).replace('.', ',')
-
   return (
     <div
       className={`flex flex-col items-center overflow-hidden rounded-xl font-bold ${
@@ -17,9 +14,7 @@ export const DataCard: React.FC<Props> = ({ title, data, isLast }) => {
       <h3 className="w-full bg-green-500 p-2 text-center text-sm tracking-wide text-neutral-800">
         {title.toUpperCase()}
       </h3>
-      <p className="w-full bg-neutral-800 p-5 text-center text-2xl">
-        {formattedData}
-      </p>
+      <p className="w-full bg-neutral-800 p-5 text-center text-2xl">{data}</p>
     </div>
   )
 }
