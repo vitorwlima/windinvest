543995317daa037e8e7abd8b42f125b1af4fb26e
Vitor Lima <vitorwlima13@gmail.com>
2023-09-01 21:58:18 -0300

    feat: improve header space for mobile

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { ChartBarSquareIcon, UserCircleIcon } from '@heroicons/react/24/outline'
+import { ChartBarSquareIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 import { SearchAsset } from './SearchAsset'
 
@@ -7,12 +7,13 @@ export const Header: React.FC = () => {
     <header className="w-full border-b border-neutral-500 px-4 py-8">
       <div className="mx-auto flex max-w-6xl items-center justify-between gap-8">
         <Link className="flex items-center gap-2" href="/">
-          <ChartBarSquareIcon className="h-8 w-8 text-green-500" />
-          <h1 className="text-4xl font-bold tracking-wider">Stockz</h1>
+          <ChartBarSquareIcon className="h-6 w-6 text-green-500 sm:h-8 sm:w-8" />
+          <h1 className="text-3xl font-bold tracking-wider sm:text-4xl">
+            Stockz
+          </h1>
         </Link>
         <section className="flex w-full max-w-md items-center gap-8">
           <SearchAsset />
-          <UserCircleIcon className="h-12 w-12" />
         </section>
       </div>
     </header>
