bfe5387e6f1a4c398119d90cb77843490a39f01f
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-13 20:24:10 -0300

    refactor: use zod for route query and params verification

--- server/src/routes/getAllAssets.ts
+++ server/src/routes/getAllAssets.ts
@@ -1,44 +1,47 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from 'src/lib/prisma'
+import { z } from 'zod'
 
 export const getAllAssets = async (fastify: FastifyInstance) => {
-  fastify.get<{ Querystring: { search: string } }>(
-    '/assets',
-    async (request, reply) => {
-      const { search } = request.query
-      try {
-        const stocks = await prisma.asset.findMany({
-          orderBy: {
-            ticker: 'asc',
-          },
-          select: {
-            ticker: true,
-            fantasyName: true,
-          },
-          where: {
-            OR: [
-              {
-                ticker: {
-                  contains: search,
-                  mode: 'insensitive',
-                },
-              },
-              {
-                fantasyName: {
-                  contains: search,
-                  mode: 'insensitive',
-                },
-              },
-            ],
-          },
-        })
+  fastify.get('/assets', async (request, reply) => {
+    const querySchema = z.object({
+      search: z.string().optional(),
+    })
 
-        reply.code(200)
-        return { ok: true, data: stocks }
-      } catch (error) {
-        reply.code(500)
-        return { ok: false, error }
-      }
-    },
-  )
+    const { search } = querySchema.parse(request.query)
+
+    try {
+      const stocks = await prisma.asset.findMany({
+        orderBy: {
+          ticker: 'asc',
+        },
+        select: {
+          ticker: true,
+          fantasyName: true,
+        },
+        where: {
+          OR: [
+            {
+              ticker: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+            {
+              fantasyName: {
+                contains: search,
+                mode: 'insensitive',
+              },
+            },
+          ],
+        },
+      })
+
+      reply.code(200)
+      return { ok: true, data: stocks }
+    } catch (error) {
+      reply.code(500)
+      return { ok: false, error }
+    }
+  })
 }
--- server/src/routes/getAsset.ts
+++ server/src/routes/getAsset.ts
@@ -2,29 +2,32 @@ import { B3Scraper } from 'b3-scraper'
 import { FastifyInstance } from 'fastify'
 import { getGrahamPrice } from 'src/lib/getGrahamPrice'
 import { getWindScore } from 'src/lib/getWindScore'
+import { z } from 'zod'
 
 export const getAsset = async (fastify: FastifyInstance) => {
-  fastify.get<{ Params: { ticker: string } }>(
-    '/asset/:ticker',
-    async (request, reply) => {
-      try {
-        const { ticker } = request.params
-        const stock = await B3Scraper.getStock({ ticker, showLogs: true })
+  fastify.get('/asset/:ticker', async (request, reply) => {
+    const paramsSchema = z.object({
+      ticker: z.string(),
+    })
 
-        if (!stock) {
-          reply.code(404)
-          return { ok: false, error: 'Stock not found' }
-        }
+    const { ticker } = paramsSchema.parse(request.params)
 
-        const windScore = getWindScore(stock)
-        const grahamPrice = getGrahamPrice(stock)
+    try {
+      const stock = await B3Scraper.getStock({ ticker, showLogs: true })
 
-        reply.code(200)
-        return { ok: true, data: { ...stock, windScore, grahamPrice } }
-      } catch (error) {
-        reply.code(500)
-        return { ok: false, error }
+      if (!stock) {
+        reply.code(404)
+        return { ok: false, error: 'Stock not found' }
       }
-    },
-  )
+
+      const windScore = getWindScore(stock)
+      const grahamPrice = getGrahamPrice(stock)
+
+      reply.code(200)
+      return { ok: true, data: { ...stock, windScore, grahamPrice } }
+    } catch (error) {
+      reply.code(500)
+      return { ok: false, error }
+    }
+  })
 }
