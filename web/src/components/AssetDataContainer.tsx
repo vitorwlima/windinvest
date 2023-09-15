93fafbe4e17316c9293fc9af149b8fba3e1cce07
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-15 12:21:42 -0300

    feat: update data container title on mobile

--- web/src/components/AssetDataContainer.tsx
+++ web/src/components/AssetDataContainer.tsx
@@ -8,7 +8,7 @@ export const AssetDataContainer: React.FC<Props> = ({ title, children }) => {
     <div className="mx-auto mt-12 grid max-w-6xl pt-4">
       <div className="overflow-hidden rounded-2xl border border-neutral-500">
         <header className="bg-green-500 p-4 text-center">
-          <h3 className="text-2xl font-bold">{title}</h3>
+          <h3 className="text-xl font-bold sm:text-2xl">{title}</h3>
         </header>
 
         {children}
