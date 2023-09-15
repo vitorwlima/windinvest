586bb4c65e34f840d0215745f2d8213a04e87f04
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-15 11:22:51 -0300

    redesign fundamentals

--- web/src/components/Fundamentals.tsx
+++ web/src/components/Fundamentals.tsx
@@ -1,5 +1,6 @@
 import { FormattedAsset } from 'src/utils/formatAsset'
 import { getFundamentals } from 'src/utils/getFundamentals'
+import { AssetDataContainer } from './AssetDataContainer'
 
 type Props = {
   asset: FormattedAsset
@@ -9,16 +10,10 @@ export const Fundamentals: React.FC<Props> = ({ asset }) => {
   const fundamentals = getFundamentals(asset)
 
   return (
-    <div className="mx-auto mt-12 grid max-w-6xl rounded-2xl rounded-t-none border-t-4 border-green-500 bg-neutral-800 pt-4">
-      <header className="border-b border-neutral-500">
-        <h3 className="px-4 pb-4 text-2xl font-bold">
-          Indicadores Fundamentalistas
-        </h3>
-      </header>
-
+    <AssetDataContainer title="INDICADORES FUNDAMENTALISTAS">
       {fundamentals.map(({ title, data }) => (
         <section className="border-neutral-500 p-4" key={title}>
-          <h4 className="mb-4 text-lg font-bold">
+          <h4 className="mb-4 text-lg">
             Indicadores de <span className="text-green-500">{title}</span>
           </h4>
 
@@ -29,12 +24,14 @@ export const Fundamentals: React.FC<Props> = ({ asset }) => {
                 className="flex flex-col gap-1 rounded-xl border border-neutral-500 p-4"
               >
                 <p className="text-xs sm:text-sm">{title.toUpperCase()}</p>
-                <strong className="text-lg">{value}</strong>
+                <data value={value} className="text-lg font-bold">
+                  {value}
+                </data>
               </li>
             ))}
           </ul>
         </section>
       ))}
-    </div>
+    </AssetDataContainer>
   )
 }
