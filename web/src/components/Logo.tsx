6a7e2b490c9bfeed6aae649f3a55fcc8d0f05cbc
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-20 16:46:58 -0300

    feat: improve logo

--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -13,7 +13,7 @@ export const Logo: React.FC<Props> = ({ size }) => {
     >
       <ChartBarSquareIcon className="h-6 w-6 text-green-500 sm:h-8 sm:w-8" />
       <h1 className="flex flex-col items-center">
-        <p className="text-xl font-bold leading-4 sm:text-2xl sm:leading-5">
+        <p className="text-xl font-bold leading-4 sm:text-2xl sm:leading-4">
           WIND
         </p>
         <p className="text-lg tracking-widest sm:text-xl">invest</p>
