f0ad5d5c834b96c456f232facf6c82c548d91065
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-21 19:26:52 -0300

    fix: design issue

--- web/src/components/AssetDataContainer.tsx
+++ web/src/components/AssetDataContainer.tsx
@@ -20,7 +20,7 @@ export const AssetDataContainer: React.FC<Props> = ({
     <div className="mx-auto mt-12 grid max-w-6xl px-4 pt-4">
       <div className="rounded-2xl border border-neutral-500">
         <header className="relative flex items-center justify-center gap-1 rounded-t-2xl bg-green-500 p-4">
-          <h3 className="text-xl font-bold text-neutral-800 sm:text-2xl">
+          <h3 className="text-center text-xl font-bold text-neutral-800 sm:text-2xl">
             {title}
           </h3>
 
