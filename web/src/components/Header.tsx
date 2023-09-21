a7838372aa36965a9df4b481e66334980f2985a0
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-21 19:40:35 -0300

    feat: improve logo accessibility

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,24 +1,12 @@
-'use client'
-
 import { UserButton } from '@clerk/nextjs'
-import { useGetSubscriptionData } from 'src/queries/useGetSubscriptionData'
 import { Logo } from './Logo'
 import { SearchAsset } from './SearchAsset'
 
 export const Header: React.FC = () => {
-  const { data } = useGetSubscriptionData()
-
-  const userIsPRO = !!data?.ok && data.data?.isUserPremium
-
   return (
     <header className="w-full border-b border-neutral-500 px-4 py-8">
       <div className="mx-auto flex max-w-6xl items-center justify-between gap-4">
-        <div className="flex items-end gap-1">
-          <Logo size="lg" />
-          {userIsPRO && (
-            <p className="mb-[2px] text-sm font-bold text-sky-500">PRO</p>
-          )}
-        </div>
+        <Logo size="lg" showPro />
         <section className="flex w-full max-w-md flex-1 items-center justify-end gap-4">
           <SearchAsset />
           <UserButton
--- web/src/components/Logo.tsx
+++ web/src/components/Logo.tsx
@@ -1,23 +1,36 @@
+'use client'
+
 import { ChartBarSquareIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import { useGetSubscriptionData } from 'src/queries/useGetSubscriptionData'
 
 type Props = {
   size: 'md' | 'lg'
+  showPro?: boolean
 }
 
-export const Logo: React.FC<Props> = ({ size }) => {
+export const Logo: React.FC<Props> = ({ size, showPro }) => {
+  const { data } = useGetSubscriptionData()
+
+  const userIsPRO = !!data?.ok && data.data?.isUserPremium
+
   return (
     <Link
-      className={`flex items-center gap-2 ${size === 'md' ? 'scale-75' : ''}`}
+      className={`flex items-end gap-1 outline-offset-4 outline-green-500 focus:outline ${
+        size === 'md' ? 'scale-75' : ''
+      }`}
       href="/"
     >
-      <ChartBarSquareIcon className="h-6 w-6 text-green-500 sm:h-8 sm:w-8" />
-      <h1 className="flex flex-col items-center">
-        <p className="text-xl font-bold leading-4 sm:text-2xl sm:leading-4">
-          WIND
-        </p>
-        <p className="text-lg tracking-widest sm:text-xl">invest</p>
-      </h1>
+      <div className="flex items-center gap-2">
+        <ChartBarSquareIcon className="h-6 w-6 text-green-500 sm:h-8 sm:w-8" />
+        <h1 className="flex flex-col items-center overflow-hidden">
+          <p className="text-xl font-bold sm:text-2xl">WIND</p>
+          <p className="-mt-2 text-lg tracking-widest sm:text-xl">invest</p>
+        </h1>
+      </div>
+      {showPro && !userIsPRO && (
+        <p className="mb-[2px] text-sm font-bold text-sky-500">PRO</p>
+      )}
     </Link>
   )
 }
