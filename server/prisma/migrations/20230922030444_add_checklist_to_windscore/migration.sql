1f5be0b106a482fd1225a31fd1ac5f0a55654772
Vitor Windberg <vitorwlima13@gmail.com>
2023-09-22 00:05:47 -0300

    feat: add checklist to windscore db

---
+++ server/prisma/migrations/20230922030444_add_checklist_to_windscore/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "windScores" ADD COLUMN     "checklistDebt" BOOLEAN,
+ADD COLUMN     "checklistLiquidity" BOOLEAN,
+ADD COLUMN     "checklistProfit" BOOLEAN,
+ADD COLUMN     "checklistRoe" BOOLEAN;
