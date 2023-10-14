e3d3ee9f29c1d21a0be4d7e10909517edcb97ae5
Vitor Windberg <vitorwlima13@gmail.com>
2023-10-13 22:23:43 -0300

    fix: unfound token after login issue

--- web/src/queries/useFetch.ts
+++ web/src/queries/useFetch.ts
@@ -4,12 +4,19 @@ export const useFetch = () => {
   const { getToken } = useAuth()
 
   return async (input: RequestInfo | URL, init?: RequestInit) => {
+    const token = await getToken()
+
+    if (!token) {
+      window.location.reload()
+      throw new Error('Token not found.')
+    }
+
     const opts = init || {}
 
     return fetch(input, {
       ...opts,
       headers: {
-        Authorization: `Bearer ${await getToken()}`,
+        Authorization: `Bearer ${token}`,
         ...(opts.headers || {}),
       },
     })
